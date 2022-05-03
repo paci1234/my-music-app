0600d070995ce0d14281b80f819aa0c3b94c3db2
ppetrov <ppetrov@codix.bg>
2022-05-03 23:31:15 +0300

    Fixed UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   public title = 'MY MUSIC APP';
+  album = false;
+  isReady = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.isReady = true;
+  }
 }
